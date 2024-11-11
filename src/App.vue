d75660391ea406fafa41cd41435c0e83c24cf6b8
mametakut <takuto.mameda@gmail.com>
2024-11-11 09:13:10 +0900

    feat: pictoImages import

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-/*
-//import { ref } from "vue";
+
+import { ref } from "vue";
 // import { invoke } from "@tauri-apps/api/core";
 
-const pictoImagesImport = import.meta.glob('./assets/picto/*.{png}', { eager: true });
+const pictoImagesImport = import.meta.glob('./assets/picto/*.png', { eager: true });
 const pictoImages = Object.values(pictoImagesImport).map(module => (module as { default: string }).default || (module as string));
 const currentPictoIndex = ref(0)
 const currentPictoImage = ref(pictoImages[currentPictoIndex.value])
-console.log(pictoImagesImport)
-*/
+console.log(pictoImages)
+
 </script>
 
 <template>
     <main class="container">
         <div class="left"></div>
         <div class="right">
-            <img src="./assets/picto/banana.png" alt="picto-san" />
+            <img :src="currentPictoImage" alt="picto-san" />
         </div>
     </main>
 </template>
