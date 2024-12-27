d797a951ed6f4be30f4a3e9e5c600ec608490add
mametakut <takuto.mameda@gmail.com>
2024-12-27 17:52:51 +0900

    chore: add semicolon

--- src/components/WidgetCalendar.vue
+++ src/components/WidgetCalendar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed } from 'vue';
 
 const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const todayDate = new Date();
