e21cafdc9a2b2f1633973970acad1637f9548215
yadokani389 <yadokani389@gmail.com>
2025-01-27 23:50:37 +0900

    fix: build android apk

--- package.nix
+++ package.nix
@@ -26,7 +26,7 @@ stdenv.mkDerivation (finalAttrs: {
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit (finalAttrs) pname version src;
     sourceRoot = "${finalAttrs.src.name}/${finalAttrs.cargoRoot}";
-    hash = "sha256-kRndQyUOSxxQlymfGmgN65xovRvE3kwSEg4I/Iojl1I=";
+    hash = "sha256-vuG6kW6vqsAuC61t3hTMPqu1yR+3eBVmU5bOCpQ1lrU=";
   };
 
   nativeBuildInputs = [
