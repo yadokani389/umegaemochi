9c93020c092bd2b5a2d06a646fb70643b67c50c0
mametakut <takuto.mameda@gmail.com>
2025-02-07 17:35:23 +0900

    wip 2

--- src-tauri/src/sports_news.rs
+++ src-tauri/src/sports_news.rs
@@ -1,18 +1,6 @@
-// disaster_infoのコピペ
-use chrono::{DateTime, Local};
-use std::sync::Mutex;
-use tauri::{Emitter, Manager};
-use unicode_normalization::UnicodeNormalization;
-
-// これをvueに渡したい
-struct SportNews {
-    title: String,
-    articles: Vec<String>,
-}
-
 // とりあえずrssを読み込んでみよう
-pub async fn get_sports_news() -> Vec<String> {
-    let url = "https://www.nikkansports.com/baseball/atom.xml";
+pub async fn get_sports_news(topic: String) -> Vec<String> {
+    let url = "https://www.nikkansports.com/" + &topic + "/atom.xml";
     let response = reqwest::get(url).await.unwrap();
     let response_str = response.text().await.unwrap();
 
@@ -23,18 +11,15 @@ pub async fn get_sports_news() -> Vec<String> {
         .skip(1)
         .map(|s| s.split("</title>").next().unwrap().to_string())
         .collect();
-
-    // 一旦テスト
-
-    article_titles
+    Ok(article_titles)
 }
 
 // テスト用の関数
 
-#[tokio::main]
-async fn main() {
-    let titles = get_sports_news().await;
-    for title in titles.iter() {
-        println!("{}", title);
-    }
-}
+// #[tokio::main]
+// async fn main() {
+//     let titles = get_sports_news().await;
+//     for title in titles.iter() {
+//         println!("{}", title);
+//     }
+// }
