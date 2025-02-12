4ca6afaab293c4605bf6e36b6ec94dc0eda86f50
yadokani389 <155296849+yadokani389@users.noreply.github.com>
2025-02-12 17:28:28 +0900

    feat: support auto_hide_cursor (#76)

--- src-tauri/src/state/settings.rs
+++ src-tauri/src/state/settings.rs
@@ -6,6 +6,7 @@ pub struct Settings {
     pub widget_interval: u64,
     pub using_widgets: Vec<String>,
     pub auto_fullscreen: bool,
+    pub auto_hide_cursor: bool,
     pub using_sports_news: Vec<String>,
 }
 
@@ -17,6 +18,7 @@ impl Default for Settings {
             widget_interval: 10000,
             using_widgets: crate::WIDGET_LIST.iter().map(|x| x.to_string()).collect(),
             auto_fullscreen: false,
+            auto_hide_cursor: false,
             using_sports_news: Vec::new(),
         }
     }
